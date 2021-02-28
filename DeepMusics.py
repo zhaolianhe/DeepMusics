8f96378c2cdf15f0a12221b08be36a2d59c93c41
Lianhe Zhao <76764673+CancerProfiling@users.noreply.github.com>
2021-02-28 23:19:22 +0800

    Update DeepMusics.py

--- DeepMusics.py
+++ DeepMusics.py
@@ -2,23 +2,23 @@ import torch
 import torch.nn as nn
 
 class DeepMusics(nn.Module):
-	def __init__(self, In_Nodes, Pathway_Nodes, Hidden_Nodes, Out_Nodes, Pathway_Mask):
+	def __init__(self, In_Nodes, funcitonal_Nodes, Hidden_Nodes, Out_Nodes, Pathway_Mask):
 		super(DeepMusics, self).__init__()
 		self.tanh = nn.Tanh()
-		self.pathway_mask = Pathway_Mask
-		self.sc1 = nn.Linear(In_Nodes, Pathway_Nodes)
+		self.pathway_mask = functional_Mask
+		self.sc1 = nn.Linear(In_Nodes, functional_Nodes)
 		self.sc2 = nn.Linear(Pathway_Nodes, Hidden_Nodes)
 		self.sc3 = nn.Linear(Hidden_Nodes, Out_Nodes, bias=False)
 		self.sc4 = nn.Linear(Out_Nodes+1, 1, bias = False)
 		self.sc4.weight.data.uniform_(-0.001, 0.001)
-		self.do_m1 = torch.ones(Pathway_Nodes)
+		self.do_m1 = torch.ones(functional_Nodes)
 		self.do_m2 = torch.ones(Hidden_Nodes)
 		if torch.cuda.is_available():
 			self.do_m1 = self.do_m1.cuda()
 			self.do_m2 = self.do_m2.cuda()
 
 	def forward(self, x_1, x_2):
-		self.sc1.weight.data = self.sc1.weight.data.mul(self.pathway_mask)
+		self.sc1.weight.data = self.sc1.weight.data.mul(self.functional_mask)
 		x_1 = self.tanh(self.sc1(x_1))
 		if self.training == True: 
 			x_1 = x_1.mul(self.do_m1)
